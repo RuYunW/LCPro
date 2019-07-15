53e064773230d023771339fce7b72dce9e7e397e
Ruyun Wang <42913632+RuYunW@users.noreply.github.com>
2019-07-16 00:00:28 +0800

    头发-1

--- EnC.py
+++ EnC.py
@@ -1,9 +1,28 @@
 #this is Encoder 4 LC-Pro
 
+from nltk import word_tokenize
+from gensim import corpora
+
 # Loading dataset
+def load_dataset():
+    filename = 'data/train_magic_test_in.txt'
+    with open(file = filename,mode='r') as file:
+        train_x = []
+        train_x_words=[]
+        lines = file.readlines()
+        for line in lines:
+            train_x.append(line)
+            train_x_words.append(line.split(' '))
+    print(train_x_words)
+    return train_x_words
+
+
 
 
 # Embadding  Word2Vec
+def word_to_vector(train_x_words):
+    dic = corpora.Dictionary(train_x_words[:][:])
+    print(dic[200])
 
 
 # CNN
@@ -12,5 +31,6 @@
 
 
 # don't know what to do ……
+word_to_vector(load_dataset())
 
 
