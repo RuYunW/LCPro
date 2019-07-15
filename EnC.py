9b33230661927e413ecb2597818cf6998d357036
Ruyun Wang <42913632+RuYunW@users.noreply.github.com>
2019-07-16 00:16:52 +0800

    头发-1

--- EnC.py
+++ EnC.py
@@ -2,6 +2,8 @@
 
 from nltk import word_tokenize
 from gensim import corpora
+from keras.models import Sequential
+from keras.layers.embeddings import Embedding
 
 # Loading dataset
 def load_dataset():
@@ -13,19 +15,32 @@ def load_dataset():
         for line in lines:
             train_x.append(line)
             train_x_words.append(line.split(' '))
-    print(train_x_words)
+    # print(train_x_words)
     return train_x_words
 
 
 
 
-# Embadding  Word2Vec
+# Word2Vec
 def word_to_vector(train_x_words):
     dic = corpora.Dictionary(train_x_words[:][:])
-    print(dic[200])
+    # 保存字典到文本文件
+    dic.save_as_text('dict_file.txt')
+    dic_set = dic.token2id
+    # 将单词转换为整数
+    values = []
+    for word in train_x_words[:][:]:
+        #查找每个单词在字典中的编码
+        values.append(dic_set[word])
+    return values
 
 
-# CNN
+# CNN + LSTM
+dic_len = len()
+def build_models():
+    model = Sequential()
+    model.add(Embedding(input_dim=dic_len))
+
 
 # LSTM
 
